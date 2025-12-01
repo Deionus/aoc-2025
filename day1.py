b9c26d50ad4d6dfb40c8feb731d88319d4458841
Deionus Bauer <deionus@gmail.com>
2025-11-30 23:03:47 -0700

    Uglify Day 1

--- day1.py
+++ day1.py
@@ -4,26 +4,22 @@ from utils import l
 def part1():
     dial = 50
     c = 0
-    for line in l():
-        dial = (dial + int(line[1:]) * (1 if line[0] == "R" else -1)) % 100
-        if dial == 0:
-            c += 1
+    for d, n in [(1 if l[0] == "R" else -1, int(l[1:])) for l in l()]:
+        dial = (dial + n * d) % 100
+        if dial == 0: c += 1
     return c
 
 
 def part2():
     dial = 50
     c = 0
-    for line in l():
-        rot = int(line[1:])
-        c += abs(rot // 100)
-        rot = (rot % 100) * (1 if line[0] == "R" else -1)
-        dial = dial + rot
-        if (dial <= 0 or dial > 99) and dial - rot != 0:
-            c += 1
+    for d, n in [(1 if l[0] == "R" else -1, int(l[1:])) for l in l()]:
+        c += abs(n // 100)
+        n = (n % 100) * d
+        dial = dial + n
+        if (dial <= 0 or dial > 99) and dial - n != 0: c += 1
         dial = dial % 100
     return c
-
 
 print(part1())
 print(part2())
