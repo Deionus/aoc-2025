21c0661c3c35b95a76a853fc596483fab345a6d2
Deionus Bauer <deionus@gmail.com>
2025-12-02 12:50:29 -0700

    Remove Day 2 Notes

--- day2.py
+++ day2.py
@@ -1,37 +1,3 @@
-"""
-Invalid IDs are IDs with 2 repeated parts
-55 = 5 & 5
-6464 = 64 & 64
-123123 = 123 & 123
-
-Find all the IDs in the ranges and add them up
-1 digit: None
-2 digit: 11, 22, 33... 99
-3 digit: None
-4 digit: 1010, 1111, 1212, 1313, etc.
-5 digit: none
-6 digit: 100100, 101101, etc.
-
-part2:
-Sequence must be repeated **At Least** twice
-1 digit: None
-2 digit: 11, 22, etc.
-3 digit: 111, 222, 333... 999
-4 digit: 1111 (1,1,1,1 OR 11,11)
-5 digit: 11111 (1,1,1,1,1)
-6 digit: 111111 (111,111 OR 11,11,11 OR 1,1,1,1,1,1)
-    12,12,12
-7 digit: 1111111(1,1,1,1,1,1,1)
-8 digit: 11111111 (1111,1111 OR 11,11,11,11 OR 1,1,1,1,1,1,1,1)
-9 digit: 111111111 (111,111,111 OR 1,1,1,1,1,1,1,1,1)
-10 digit: 1111111111 (11111,11111 OR 11,11,11,11,11 OR 1,1,1,1,1,1,1,1,1,1)
-
-If my range is 1000,9999
-I know my target value has 4 digits, (factors 1, 2, 4)
-Factors 2 & 4 are >= 2, so those are the repititions I should look for
-Need to be careful not to double count nums (any repition of 4 is also a rep of 2)
-"""
-
 from functools import cache
 from utils import c
 
