ec42809b4aed07880b74db7a5fa387ce7f47c2a4
Deionus Bauer <deionus@gmail.com>
2025-12-05 00:00:04 -0700

    Day 5 Ugly Edition

--- day5.py
+++ day5.py
@@ -1,19 +1,15 @@
 from utils import o
 
-input = o()
-a, b = input.split("\n\n")
-a = a.split("\n")
-b = b.split("\n")
 
+a, b = (i.split("\n") for i in o().split("\n\n"))
 ranges = sorted([(int(mini), int(maxi)) for mini, maxi in [i.split("-") for i in a]], key=lambda x: x[0])
 
 
 def part1():
     fresh = 0
     for i in b:
-        i = int(i)
         for mini, maxi in ranges:
-            if mini <= i <= maxi: fresh += 1; break
+            if mini <= int(i) <= maxi: fresh += 1; break
     return fresh
 
 
@@ -22,17 +18,11 @@ def part2():
     r = ranges[0]
     for i in range(1, len(ranges)):
         r2 = ranges[i]
-        if r2[0] <= r[1]:
-            r = (r[0], max(r2[1], r[1]))
-            i += 1
-        else:
-            new_ranges.append(r)
-            r = r2
+        if r2[0] <= r[1]: r = (r[0], max(r2[1], r[1])); i += 1
+        else: new_ranges.append(r); r = r2
     new_ranges.append(r)
     return sum(maxi-mini+1 for mini, maxi in new_ranges)
 
 
-p1 = part1()
-p2 = part2()
-print(p1)
-print(p2)
+print(part1())
+print(part2())
