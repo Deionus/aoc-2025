e27351dc1b3058c3f35c3d6b595837a01d79af95
Deionus Bauer <deionus@gmail.com>
2025-12-05 15:36:20 -0700

    ADJACENT to ADJ_8

--- day4.py
+++ day4.py
@@ -1,9 +1,9 @@
-from utils import dg, g, ADJACENT
+from utils import dg, g, ADJ_8
 
 
 def part1():
     grid = dg(lambda: ".")
-    return sum([sum([1 for a, b in ADJACENT if grid[(a+row, b+col)] == "@"]) < 4 for (row, col), val in list(grid.items()) if val == "@"])
+    return sum([sum([1 for a, b in ADJ_8 if grid[(a+row, b+col)] == "@"]) < 4 for (row, col), val in list(grid.items()) if val == "@"])
 
 
 def part2():
@@ -17,7 +17,7 @@ def part2():
         for row in range(N):
             for col in range(M):
                 if grid[row][col] == ".": continue
-                r = sum([1 for x, y in ((row+a, col+b) for a,b in ADJACENT) if x >= 0 and x < N and y >= 0 and y < M and grid[x][y] == "@"]) < 4
+                r = sum([1 for x, y in ((row+a, col+b) for a,b in ADJ_8) if x >= 0 and x < N and y >= 0 and y < M and grid[x][y] == "@"]) < 4
                 removed += r
                 total += r
                 if r: grid[row][col] = "."
