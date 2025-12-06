f007a71df679a9f4294e93edc40999c5ebf3c99a
Deionus Bauer <deionus@gmail.com>
2025-12-06 14:05:59 -0700

    Use Runner for Day 3

--- day3.py
+++ day3.py
@@ -1,6 +1,5 @@
-from utils import l
+from runner import go
 
-BANKS = [[int(i) for i in list(a)] for a in l()]
 
 def calculate(volts, batteries):
     left = 0
@@ -11,5 +10,17 @@ def calculate(volts, batteries):
         left += volts[left:p+1].index(q[-1]) + 1
     return int("".join(str(i) for i in q))
 
-print(sum(calculate(volts, 2) for volts in BANKS))
-print(sum(calculate(volts, 12) for volts in BANKS))
+
+def process(input):
+    return [[int(i) for i in list(a)] for a in input.split("\n")]
+
+
+def part1(input):
+    return sum(calculate(volts, 2) for volts in input)
+
+
+def part2(input):
+    return sum(calculate(volts, 12) for volts in input)
+
+
+go(process, part1, part2)
