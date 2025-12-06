42eeea90c87717e4e6bb80da4c308f375f7efab7
Deionus Bauer <deionus@gmail.com>
2025-12-06 14:02:16 -0700

    Use Runner for Day 1

--- day1.py
+++ day1.py
@@ -1,19 +1,23 @@
-from utils import l
+from runner import go
 
 
-def part1():
+def process(input):
+    return input.split("\n")
+
+
+def part1(input):
     dial = 50
     c = 0
-    for d, n in [(1 if l[0] == "R" else -1, int(l[1:])) for l in l()]:
+    for d, n in [(1 if l[0] == "R" else -1, int(l[1:])) for l in input]:
         dial = (dial + n * d) % 100
         if dial == 0: c += 1
     return c
 
 
-def part2():
+def part2(input):
     dial = 50
     c = 0
-    for d, n in [(1 if l[0] == "R" else -1, int(l[1:])) for l in l()]:
+    for d, n in [(1 if l[0] == "R" else -1, int(l[1:])) for l in input]:
         c += abs(n // 100)
         n = (n % 100) * d
         dial = dial + n
@@ -21,5 +25,5 @@ def part2():
         dial = dial % 100
     return c
 
-print(part1())
-print(part2())
+
+go(process, part1, part2)
