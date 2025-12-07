8579a3bd39a63dfb470796e8e232ccf80e1266e9
Deionus Bauer <deionus@gmail.com>
2025-12-06 21:48:02 -0700

    Day 6 edits

--- day6.py
+++ day6.py
@@ -7,6 +7,6 @@ def part1(input):
     return sum(eval(p[-1].join(p[:-1])) for p in zip(*[[i for i in l.split(" ") if i] for l in input]))
 
 def part2(input):
-    return sum([eval(p[0].replace(":", p[1])) for p in zip(":".join(["".join(a).strip().replace(" ", "0") for a in zip(*[reversed(l) for l in input[:-1]])]).split("::"), reversed([op for op in input[-1].split(" ") if op]))])
+    return sum([eval(p[0].replace(":", p[1])) for p in zip(":".join(["".join(a).strip() for a in zip(*input[:-1])]).split("::"), [op for op in input[-1].split(" ") if op])])
 
 go(process, part1, part2)
