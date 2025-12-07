193f23455a9f53787b01e3a259e2a00cbc67ee48
Deionus Bauer <deionus@gmail.com>
2025-12-06 21:48:27 -0700

    Update template

--- template.py
+++ template.py
@@ -1,12 +1,16 @@
 from runner import go
 
-def process(input):
+
+def process(input: str):
     return input
+
 
 def part1(input):
     return 0
 
+
 def part2(input):
     return 0
+
 
 go(process, part1, part2)
