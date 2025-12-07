a8fa6ef9b6c34edab0b1286cd2a90f65c384fa02
Deionus Bauer <deionus@gmail.com>
2025-12-06 22:53:25 -0700

    Day 7 Swagged Out

--- day7.py
+++ day7.py
@@ -10,27 +10,19 @@ def part1(input):
     beams = {input[0].index("S")}
     splits = 0
     for line in input[1:-1]:
-        to_add = set()
-        for b in beams:
-            if line[b] == "^":
-                to_add.add(b-1)
-                to_add.add(b+1)
-                splits += 1
-            else:
-                to_add.add(b)
-        beams = to_add
+        new = [{b-1, b+1} if line[b] == "^" else {b} for b in beams]
+        splits += sum(len(n) > 1 for n in new)
+        beams = set().union(*new)
     return splits
 
 
 def part2(input):
-    beams = collections.defaultdict(int)
-    beams[input[0].index("S")] = 1 
+    beams = collections.defaultdict(int, {input[0].index("S"): 1})
     for line in input[1:-1]:
         for b in list(beams.keys()):
             if line[b] == "^":
-                v = beams[b]
-                beams[b-1] += v
-                beams[b+1] += v
+                beams[b-1] += beams[b]
+                beams[b+1] += beams[b]
                 beams[b] = 0
     return sum(beams.values())
 
