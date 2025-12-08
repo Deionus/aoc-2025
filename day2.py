bc4ed070913ee634e9ced1b1378c00b13b2e520c
Deionus Bauer <deionus@gmail.com>
2025-12-07 18:25:07 -0700

    Use Runner for Day 2

--- day2.py
+++ day2.py
@@ -1,19 +1,7 @@
 from functools import cache
 from utils import c
+from runner import go
 
-RANGES = sorted([(int(a), int(b)) for a, b in [r.split("-") for r in c()]], key=lambda x: x[0])
-def solve(generator, skip):
-    total = 0
-    n = 1
-    i = iter(RANGES)
-    r = next(i)
-    while r:
-        if skip(n): n+=1; continue
-        num = generator(n)
-        if num >= r[0] and num <= r[1]: total += num
-        elif num > r[1]: r = next(i, None); continue
-        n += 1
-    return total
 
 @cache
 def digits(num):
@@ -23,17 +11,39 @@ def digits(num):
         num = num // 10
     return i
 
-totals = [
-    solve(gen, skip) for gen, skip in 
-    [
-        (lambda n: (n*(10**digits(n)))+n, lambda _: False),
-        (lambda n: (n*(100**digits(n))) + (n*(10**digits(n))) + n, lambda n: n % 11 == 0),
-        (lambda n: (n*(10000**digits(n))) + (n*(1000**digits(n))) + (n*(100**digits(n))) + (n*(10**digits(n))) + n, lambda n: n % 11 == 0),
-        (lambda n: (n*(1000000**digits(n))) + (n*(100000**digits(n))) + (n*(10000**digits(n))) + (n*(1000**digits(n))) + (n*(100**digits(n))) + (n*(10**digits(n))) + n, lambda _: False),
-    ]
+
+def solve(ranges, generator, skip):
+    total = 0
+    n = 1
+    i = iter(ranges)
+    r = next(i)
+    while r:
+        if skip(n): n+=1; continue
+        num = generator(n)
+        if num >= r[0] and num <= r[1]: total += num
+        elif num > r[1]: r = next(i, None); continue
+        n += 1
+    return total
+
+
+GENERATORS = [
+    (lambda n: (n*(10**digits(n)))+n, lambda _: False),
+    (lambda n: (n*(100**digits(n))) + (n*(10**digits(n))) + n, lambda n: n % 11 == 0),
+    (lambda n: (n*(10000**digits(n))) + (n*(1000**digits(n))) + (n*(100**digits(n))) + (n*(10**digits(n))) + n, lambda n: n % 11 == 0),
+    (lambda n: (n*(1000000**digits(n))) + (n*(100000**digits(n))) + (n*(10000**digits(n))) + (n*(1000**digits(n))) + (n*(100**digits(n))) + (n*(10**digits(n))) + n, lambda _: False),
 ]
 
-part1 = totals[0]
-part2 = sum(totals)
-print(part1)
-print(part2)
+
+def process(input: str):
+    return sorted([(int(a), int(b)) for a, b in [r.split("-") for r in input.split(",")]], key=lambda x: x[0])
+
+
+def part1(input):
+    return sum(solve(input, gen, skip) for gen, skip in GENERATORS[:1])
+
+
+def part2(input):
+    return sum(solve(input, gen, skip) for gen, skip in GENERATORS)
+
+
+go(process, part1, part2)
