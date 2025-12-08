eac91ba580e27b4df5b485915637cedd3a583a44
Deionus Bauer <deionus@gmail.com>
2025-12-07 20:14:58 -0700

    Day 4 edits

--- day4.py
+++ day4.py
@@ -12,23 +12,20 @@ def part1(input):
 
 
 def part2(input):
-    grid = input
     total = 0
-    N = len(grid)
-    M = len(grid[0])
+    N = len(input)
+    M = len(input[0])
 
-    removed = 0
-    while True:
+    removed = 1
+    while removed:
+        removed = 0
         for row in range(N):
             for col in range(M):
-                if grid[row][col] == ".": continue
-                r = sum([1 for x, y in ((row+a, col+b) for a,b in ADJ_8) if x >= 0 and x < N and y >= 0 and y < M and grid[x][y] == "@"]) < 4
+                if input[row][col] == ".": continue
+                r = sum(1 for x, y in ((row+a, col+b) for a,b in ADJ_8) if x >= 0 and x < N and y >= 0 and y < M and input[x][y] == "@") < 4
                 removed += r
                 total += r
-                if r: grid[row][col] = "."
-        
-        if not removed: break
-        removed = 0
+                if r: input[row][col] = "."
 
     return total
 
