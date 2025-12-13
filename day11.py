6c7b97a3c7c14bc0d8b6281726904b61aec7ed8b
Deionus Bauer <deionus@gmail.com>
2025-12-13 13:49:27 -0700

    Day 11 Part 2 Cached DFS

--- day11.py
+++ day11.py
@@ -1,4 +1,5 @@
 import collections
+from functools import cache
 from runner import go
 
 
@@ -22,39 +23,14 @@ def part1(input):
 def part2(input):
     map_ = {a: b for a, b in input}
 
-    reversed = collections.defaultdict(set)
-    for a, b in map_.items():
-        for i in b:
-            reversed[i].add(a)
-
-    parent_count = {
-        k: len(v) for k, v in reversed.items()
-    }
-
-    ways_to_get = {"svr": (1, 0, 0)}
-    q = collections.deque(map_["svr"])
-    while q:
-        node = q.popleft()
-        parent_count[node] -= 1
-
-        if parent_count[node] == 0:
-            total_ways, fft_ways, dac_ways = 0, 0, 0
-            for n in reversed[node]:
-                a, b, c = ways_to_get[n]
-                total_ways += a
-                fft_ways += b
-                dac_ways += c
-            
-            if node == "fft":
-                fft_ways = total_ways
-            elif node == "dac":
-                dac_ways = fft_ways
-            
-            ways_to_get[node] = (total_ways, fft_ways, dac_ways)
+    @cache
+    def dfs(node, dac, fft):
+        if node == "out":
+            return dac and fft
         
-            q.extend(map_.get(node) or [])
+        return sum(dfs(n, dac or node == "dac", fft or node == "fft") for n in map_[node])
     
-    return ways_to_get["out"][2]
+    return sum(dfs(n, False, False) for n in map_["svr"])
 
 
 go(process, part1, part2)
